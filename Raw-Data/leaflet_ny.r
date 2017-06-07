960dfd7aebe1b736b953f5a070dda71e9283f2ab
Cristian E. Nuno <nuno.e.cristian@gmail.com>
2017-06-06 22:18:56 -0400

    Dynamic Legend Not Working

--- Raw-Data/leaflet_ny.r
+++ Raw-Data/leaflet_ny.r
@@ -9,66 +9,92 @@ library( magrittr )
 library( htmltools )
 library( htmlwidgets )
 library( geojsonio )
+library( scales )
 # Load necessary data frames
-rds_url <- "https://github.com/DataCapstone/Data-Capstone/blob/master/Raw-Data/NYgra16.rds?raw=true"
-fy16_ny_grants <- readRDS( gzcon( url( rds_url ) ) ) # 5 seconds
-geojson_url <- "https://raw.githubusercontent.com/DataCapstone/Data-Capstone/master/Raw-Data/NY_counties.geojson"
+geojson_url <- "https://raw.githubusercontent.com/DataCapstone/Data-Capstone/master/Raw-Data/ny_county_code.geojson"
 ny_counties <- geojson_read( geojson_url, what = "sp" )
 
-# create a county code id from the geoid
-# inside the geojson file
-county_code <- as.character( ny_counties$geoid )
-length( county_code ) # 62 county codes
-county_code <- unlist( strsplit( county_code
-                 , split = "05000US36"
-                 , fixed = TRUE
-                 )
+# what are the breaks
+bins <- c(0, 1000000, 5000000
+          , 10000000, 25000000, 50000000, 100000000, Inf)
+bin_pc <- c(0, 25, 50
+            , 100, 500, 1000, 2000, Inf)
+# Design a color palette for the map
+pal <- colorBin( palette = "YlGnBu"
+                 , domain = ny_counties$federal_funding
+                 , bins = bins
 )
-length( county_code ) # 124..looks like the odds are blank
-# extract evens
-county_code <- county_code[ seq( from = 2, to = length(county_code), by = 2 )]
-# class 
-class( county_code )
-county_code <- as.numeric( county_code )
-# save to geojson
-ny_counties$county_code <- county_code
-# We might want just the county name
-# not the " County, NY"
-county_name <- unlist( strsplit( as.character(ny_counties$name)
-                          , split = " County, NY"
-                          , fixed = TRUE
-                          )
-                )
-# add to geojson file
-ny_counties$county_name <- county_name
-# save as geojson
-geojson_write( ny_counties
-               , file = "/Users/cristiannuno/Desktop/Syracuse/MPA_Portfolio/DATA_ACT/data_export/ny_county_code.geojson"
-               )
-# import revised geojson
-update_url <- "https://raw.githubusercontent.com/DataCapstone/Data-Capstone/master/Raw-Data/ny_county_code.geojson"
-update_map <- geojson_read( update_url, what = "sp")
+pal_pc <- colorBin( palette = "YlGnBu"
+                    , domain = ny_counties$funding_per_capita
+                    , bins = bin_pc
+)
+# Make hover layer
+labels_fed_fund <- sprintf(
+  "<strong>%s</strong><br/>%s dollars",
+  ny_counties$name, dollar(ny_counties$federal_funding)
+) %>% lapply(htmltools::HTML)
+labels_fed_fund_pc <- sprintf(
+  "<strong>%s</strong><br/>%s dollars",
+  ny_counties$name, dollar(ny_counties$funding_per_capita)
+) %>% lapply(htmltools::HTML)
 # make map
-ny_map <- leaflet() %>%
-  # set view to cover the state of New York
-  fitBounds( lng1 = -72, lat1 = 46
+ny_map <- leaflet(ny_counties) %>%
+  # set zoom level
+  setView( lng = -76.126197, lat = 43.034706
+           , zoom = 7) %>%
+  # set max bounds view to cover the state of New York
+  setMaxBounds( lng1 = -72, lat1 = 46
              , lng2 = -80, lat2 = 40  ) %>% 
-  # add Polygons
-  addPolygons( data = update_map
-              , smoothFactor = 0.2
-              , fillOpacity = 0.7
-              # Whenever someone hovers their mouse above a polygon,
-              # make sure they know which community they are in
-              , popup = as.character(update_map$county_code)
-              , label = update_map$county_name
-              , labelOptions = labelOptions( textsize = "25px"
-                                             , textOnly = TRUE
-                                             )
-              , highlightOptions = highlightOptions( color = "orange"
-                                                     , weight = 6
-                                                     , bringToFront = TRUE
-              )
-              ) %>%
+  # add Total FY16 Federal Grant Spending Polygons
+  addPolygons( smoothFactor = 0.2
+               , fillOpacity = 0.7
+               , fillColor = ~pal(ny_counties$federal_funding)
+               , color = "white"
+               # Whenever someone hovers their mouse above a polygon,
+               # make sure they know which community they are in
+               #, popup = dollar( update_map$federal_funding )
+               , label = labels_fed_fund
+               , labelOptions = labelOptions(style = list("font-weight" = "normal"
+                              , padding = "3px 8px")
+                              , textsize = "15px", direction = "auto"
+                 )
+               , highlightOptions = highlightOptions( color = "orange"
+                                                      , weight = 6
+                                                      , bringToFront = TRUE
+               )
+               , group = "Total Spending"
+  ) %>%
+  # add Per Capita Total FY16 Federal Grant Spending Polygons
+  addPolygons( smoothFactor = 0.2
+               , fillOpacity = 0.7
+               , fillColor = ~pal_pc(ny_counties$funding_per_capita)
+               , color = "white"
+               # Whenever someone hovers their mouse above a polygon,
+               # make sure they know which community they are in
+               #, popup = dollar( update_map$federal_funding )
+               , label = labels_fed_fund_pc
+               , labelOptions = labelOptions(style = list("font-weight" = "normal"
+                                                          , padding = "3px 8px")
+                                             , textsize = "15px", direction = "auto"
+               )
+               , highlightOptions = highlightOptions( color = "orange"
+                                                      , weight = 6
+                                                      , bringToFront = TRUE
+               )
+               , group = "Per Capita Spending"
+  ) %>%
+  # add Layers control
+  addLayersControl( baseGroups = c("Total Spending", "Per Capita Spending")
+                    , options = layersControlOptions(collapsed = FALSE)
+                    ) %>%
+  # Make sure people know what the color values indicate
+  addLegend("bottomleft"
+            , pal = pal_pc # use the same color palette we made earlier
+            , values = ny_counties$funding_per_capita # assign values to the legend
+            , title = "Per Capita  Total FY16 Federal Grant by County (without State Gov't as a Recipient)"
+            , labFormat = labelFormat(prefix = "$")
+            , opacity = 1
+  ) %>%
   # add background to map
   addProviderTiles(providers$Esri.WorldStreetMap) %>%
   # add mini map
@@ -80,8 +106,10 @@ ny_map <- leaflet() %>%
   # add zoom out button
   addEasyButton( easyButton(
     icon = "ion-android-globe", title = "Zoom Back Out"
-    , onClick = JS("function(btn, map){ map.setZoom(6); }")
+    , onClick = JS("function(btn, map){ map.setZoom(7); }")
   ) )
 # view map
 ny_map
+
+
 
