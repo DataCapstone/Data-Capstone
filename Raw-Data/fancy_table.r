95eed40222f9b88f87d7b1e2c3f41843562de623
Cristian E. Nuno <nuno.e.cristian@gmail.com>
2017-06-08 09:25:54 -0400

    Create fancy_table.r

---
+++ Raw-Data/fancy_table.r
@@ -0,0 +1,22 @@
+#
+# Author:   Cristian Nuno
+# Date:     June 7, 2017
+# Purpose:  Datatable Creative
+#
+#
+# Load necessary packages
+library( DT )
+library( magrittr )
+library( dplyr )
+
+# Create the data table
+fancy_table <- datatable( select( ny_counties@data
+                                  , county_name
+                                  , federal_funding
+                                  , funding_per_capita
+)
+  , rownames = FALSE
+  , colnames = c("County Name", "Total Federal Funding", "Total Per Capita Federal Funding")
+  , options = list(lengthChange = FALSE)
+  ) %>% # end of creating datatable
+   formatCurrency(columns = c("federal_funding", "funding_per_capita"))
