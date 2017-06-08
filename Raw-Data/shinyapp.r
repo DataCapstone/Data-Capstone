226721aa18586a7295559d351c131bcb871e10ab
Cristian E. Nuno <nuno.e.cristian@gmail.com>
2017-06-08 09:28:38 -0400

    Update shinyapp.r

--- Raw-Data/shinyapp.r
+++ Raw-Data/shinyapp.r
@@ -1,16 +1,11 @@
 #
 # Author:   Cristian Nuno
 # Date:     June 7, 2017
-# Purpose:  Using Shiny + Leaflet for FY16 NY Federal Spending
+# Purpose:  Draft Dashboard
 #
 # Load necessary packages
-library( leaflet )
-library( magrittr )
-library( htmltools )
-library( htmlwidgets )
-library( geojsonio )
-library( scales )
 library( shiny )
+library( shinydashboard )
 
 # Import data from github function
 source_github <- function( url ) {
@@ -21,22 +16,49 @@ source_github <- function( url ) {
   script <- getURL(url, ssl.verifypeer = FALSE)
   eval(parse(text = script), envir=.GlobalEnv)
 } 
-
-# Import data!
+# Import leaflet
 leaflet_url <- "https://raw.githubusercontent.com/DataCapstone/Data-Capstone/master/Raw-Data/leaflet_ny.r"
 source_github( leaflet_url )
+# Import table
+fancy_table_url <- "https://raw.githubusercontent.com/DataCapstone/Data-Capstone/master/Raw-Data/fancy_table.r"
+source_github( fancy_table_url )
+# A dashboard has three parts: a header, a sidebar, and a body. 
+# Here’s the most minimal possible UI for a dashboard page.
 
-# Make the shiny app!
-ui <- fluidPage( 
-  h1("FY16 New York Federal Awards Received by County", align = "center")
-  , h2("At first glance, it appears that counties with universities receive the most federal awards.\n
-       However, accounting for population, the trend changes"
-       )
-  , leafletOutput('mymap', height=600)
-  , h2("See more graphics, down below!")
+## customize header ##
+header <- dashboardHeader(title = "FY16 New York"
+                          #, titleWidth = 200
+                  # ,  # Dropdown menu for messages
+                  # dropdownMenu(type = "messages", badgeStatus = "success",
+                  #              messageItem("Support Team",
+                  #                          "This is the content of a message.",
+                  #                          time = "5 mins"
+                  #              )
+                  # )
+  )
+## customize sidebar ##
+sidebar <- dashboardSidebar( disable = TRUE)
+
+## customize body ##
+body <- dashboardBody(
+  fluidRow(
+    column(width = 4
+           , DT::dataTableOutput('tbl')
+           )
+    , column( width = 8
+              , leafletOutput('mymap', height=600)
+              )
+) )
+
+## Shiny UI ##
+ui <- dashboardPage(
+  header
+  , sidebar
+  , body
 )
 
-server <- function(input, output, session) {
+## server ##
+server <- function(input, output) {
   
   observeEvent(input$mymap_groups,{
     
@@ -44,28 +66,37 @@ server <- function(input, output, session) {
     
     if (input$mymap_groups == "Total Spending")
     {mymap <- mymap %>% addLegend("bottomleft"
-                                      , pal = pal # use the same color palette we made earlier
-                                      , values = ny_counties$federal_funding # assign values to the legend
-                                      , title = "Total Federal Grant Spending"
-                                      , labFormat = labelFormat(prefix = "$")
-                                      , opacity = 1
+                                  , pal = pal # use the same color palette we made earlier
+                                  , values = ny_counties$federal_funding # assign values to the legend
+                                  , title = "Total Federal Grant Spending"
+                                  , labFormat = labelFormat(prefix = "$")
+                                  , opacity = 1
     )} # end of if statement
     else if (input$mymap_groups == "Per Capita Spending")
     {mymap <- mymap %>% addLegend("bottomleft"
-                                    , pal = pal_pc # use the same color palette we made earlier
-                                    , values = ny_counties$funding_per_capita # assign values to the legend
-                                    , title = "Per Capita Federal Grant Spending"
-                                    , labFormat = labelFormat(prefix = "$")
-                                    , opacity = 1
+                                  , pal = pal_pc # use the same color palette we made earlier
+                                  , values = ny_counties$funding_per_capita # assign values to the legend
+                                  , title = "Per Capita Federal Grant Spending"
+                                  , labFormat = labelFormat(prefix = "$")
+                                  , opacity = 1
     ) } # end of else if statement
-    }
-    ) # end of observe event
+  }
+  ) # end of observe event
   
   # Render the map
   output$mymap <- renderLeaflet({
     ny_map
-  })
+    
+  }) # end of render map
+  
+  # Create the data table
+  output$tbl <- DT::renderDataTable({
+    fancy_table
+    
+  }) # end of render datatable
+
+
 } # end of server
 
-# run shiny app
-shinyApp( ui, server)
+## call the Shiny App ##
+shinyApp(ui, server)
