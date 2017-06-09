33ddb16de46bee18abbf754ee05775999b1069b5
icps86 <icps86>
2017-06-09 10:47:09 -0400

    HH

--- Ignacio/census comparator.R
+++ Ignacio/census comparator.R
@@ -4,10 +4,9 @@
 library(ggplot2)
 library(dplyr)
 
-#CENSUS 
-cen <- readRDS( gzcon(url("https://github.com/DataCapstone/Data-Capstone/blob/master/Raw-Data/NYcensus.rds?raw=true")))
+########CREATING A NORMALIZED CENSUS DF#######################
+#dat <- readRDS( gzcon(url("https://github.com/DataCapstone/Data-Capstone/blob/master/Raw-Data/NYcensus.rds?raw=true")))
 
-dat <- cen 
 dat <- arrange(dat, county.name)
 dat$county.name <- as.character(dat$county.name)
 
@@ -32,38 +31,66 @@ dat <- rbind(dat,y)
 dat[,2:3]<- round(dat[,2:3], digits = 0)
 dat[,4]<- round(dat[,4]*100, digits = 1)
 
-saveRDS(dat, "NYcen_norm.RDS")
-dir()
+#saveRDS(dat, "NYcen_norm.RDS")
 
 
+###########################################
+
+dat <- readRDS( gzcon(url("https://github.com/DataCapstone/Data-Capstone/blob/master/Raw-Data/NYcen_norm.RDS?raw=true")))
 
 #only working with four comparatos and NY state
 #"Matches for Onondaga: 1.Broome, 2.St. Lawrence, 3.Orange, 4.Sullivan, 5.Monroe"
-x <- dat$county.name %in% c("Onondaga", "Broome", "St. Lawrence", "Sullivan", "State Average")
+x <- dat$county.name %in% c("Onondaga", "Broome", "St. Lawrence", "Sullivan")
 dat <- dat[x,]
 dat$county.name <- factor(dat$county.name, ordered= TRUE)
-
+rownames(dat) <- 1:nrow(dat)
 
 #################### MAKING THE BARPLOT #######################
 
-dat
-#dat$Pop <- c(1,8,18,14,10) dat$MHincome <- c(11,19,4,12,10) dat$pov.rate <- c(5,8,14,17,10)
+tags <- dat[1:4]
 
+dat <- dat[1:nrow(dat)-1,]
 
 #using tiles
-bars <- ggplot(dat, aes(fill=county.name, height=.4, width=.5)) +
-  geom_tile(aes(x=dat$Pop, y="Population"), stat="identity") +
-  geom_tile(aes(x=dat$MHincome, y="Median Home Income"), stat="identity") + 
-  geom_tile(aes(x=dat$pov.rate, y="Poverty Rate"), stat="identity") +
-  geom_hline(yintercept=c(2.5,1.5)) +
+bars <- ggplot(dat, aes(fill=county.name, height=.4, width=.05)) +
+  geom_vline(xintercept=0, color = "grey70", size=2, linetype = "solid") +
+  geom_tile(aes(x=dat$Pop.n, y="Population"), stat="identity") +
+  geom_tile(aes(x=dat$MHincome.n, y="Median Home Income"), stat="identity") + 
+  geom_tile(aes(x=dat$pov.rate.n, y="Poverty Rate"), stat="identity") +
+  geom_hline(yintercept=c(2.5,1.5), size = 3, color = "white" ) +
   theme(legend.position = "top",
         axis.title = element_blank(),
         panel.border = element_blank(),
-        legend.title = element_blank()
-        )
+        legend.title = element_blank())
 bars
 
 
+
+
+###################FUNCTION#########################
+x<- dat
+
+krzycensus <- function (x)
+  {
+  # Load Required Packages
+  require(ggplot2)
+  
+  #making pot
+  bars <- ggplot(x, aes(fill=county.name, height=.4, width=.05)) +
+    geom_vline(xintercept=0, color = "grey70", size=2, linetype = "solid") +
+    geom_tile(aes(x=dat$Pop.n, y="Population"), stat="identity") +
+    geom_tile(aes(x=dat$MHincome.n, y="Median Home Income"), stat="identity") + 
+    geom_tile(aes(x=dat$pov.rate.n, y="Poverty Rate"), stat="identity") +
+    geom_hline(yintercept=c(2.5,1.5), size = 3, color = "white" ) +
+    theme(legend.position = "top",
+          axis.title = element_blank(),
+          panel.border = element_blank(),
+          legend.title = element_blank())
+  return(bars)
+   
+}
+
+krzycensus(x)
 
 
 ############################################
@@ -74,38 +101,12 @@ my_grob = grobTree(textGrob("This text is at x=0.1 and y=0.9, relative!\n Anchor
                             gp=gpar(col="firebrick", fontsize=25, fontface="bold")))
 ggplot(mtcars, aes(x=cyl)) + geom_bar() + annotation_custom(my_grob) + labs(title="Annotation Example")
 
+geom_text(aes(x= (width.min+width.max)/2, y = x$mid, label = paste0(round(x$fraction*100, digits = 1),"%")),
+          size= percent.cex,
+          fontface = "bold",
+          hjust = .5,
+          vjust = .5,
+          colour = "white") +
+
 
 ##############################################
-
-#DONUT
-
-pie <- ggplot(data=x,
-              aes(fill=labels,
-                  xmax=width.max, xmin=width.min,
-                  ymax=ymax, ymin=ymin)) +
-  xlim(c(0, width.max + mar)) +
-  geom_rect(colour=border.col,
-            size = border.cex) +
-  coord_polar(theta="y") +
-  geom_text(aes(x= (width.min+width.max)/2, y = x$mid, label = paste0(round(x$fraction*100, digits = 1),"%")),
-            size= percent.cex,
-            fontface = "bold",
-            hjust = .5,
-            vjust = .5,
-            colour = "white") +
-  ggtitle(label=main) +
-  theme_bw() +
-  theme(panel.grid=element_blank(),
-        axis.text=element_blank(), 
-        axis.ticks=element_blank(),
-        axis.title = element_blank(),
-        panel.border = element_blank(),
-        strip.background = element_rect(fill= sbgfill, color = sbgcol), 
-        plot.margin = margin(0,0,0,0, "cm"),
-        legend.margin = margin(0,0,0,0, "cm"),
-        legend.title = element_blank(),
-        legend.position = "right",
-        legend.text = element_text(colour = "grey40", size = 9)) +
-  facet_wrap(~multiple, ncol = columns)
-return(pie)
-}
---
+++ Ignacio/census_comp.R
@@ -0,0 +1,32 @@
+#################LOADING THE FUNCTION##########################
+
+source_github <- function( url ) {
+  # load package
+  require(RCurl)
+  
+  # read script lines from website and evaluate
+  script <- getURL(url, ssl.verifypeer = FALSE)
+  eval(parse(text = script), envir=.GlobalEnv)
+} 
+
+#load the donutzz function using the RAW link
+source_github("https://raw.githubusercontent.com/icps86/Functions/master/krzycensuz.r")
+
+
+#################FORMATTING THE DF##########################
+
+dat <- readRDS( gzcon(url("https://github.com/DataCapstone/Data-Capstone/blob/master/Raw-Data/NYcen_norm.RDS?raw=true")))
+
+#only working with four comparatos and NY state
+#"Matches for Onondaga: 1.Broome, 2.St. Lawrence, 3.Orange, 4.Sullivan, 5.Monroe"
+x <- dat$county.name %in% c("Onondaga", "Broome", "St. Lawrence", "Sullivan", "State Average")
+dat <- dat[x,]
+dat$county.name <- factor(dat$county.name, ordered= TRUE)
+rownames(dat) <- 1:nrow(dat)
+
+#################### MAKING THE BARPLOT #######################
+
+
+krzycensuz(dat)
+
+
